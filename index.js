c9864dbe2d64856e9200d9e0f5f2105b3cc55fca
Steffen Larsen <lillelarsen82@gmail.com>
2020-03-03 15:36:55 +0100

    corrected cors settings again

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
-// CONFIG
 const cors = require('cors')
 
-const corsOptions = {
-  origin: 'https://portfolio-af45a.web.app/*'
-}
-
-cors(corsOptions)
+// CONFIG
+app.use(cors({
+  origin: 'https://portfolio-af45a.web.app/*',
+  methods: ['GET']
+}));
 
 require('./config/parser')(app);
 
